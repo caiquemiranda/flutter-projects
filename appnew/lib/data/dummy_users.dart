8c7ea6546d9f65a4b7e1d0fbdcfab9d41f2db4da
caiquemiranda <caiquemirandas@gmail.com>
2022-04-28 20:34:51 -0300

    many-problems

---
+++ appnew/lib/data/dummy_users.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_crud/models/users.dart';
+
+const DUMMY_USERS = {
+  '1' : const User(
+    id: '1',
+    name: 'John Doe',
+    email: 'johndoe@aluno.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png',
+  ),
+
+  '2' : const User(
+    id: '2',
+    name: 'Jane Doe',
+    email: 'janedoe@aluno.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2021/01/18/08/32/naruto-5927441_960_720.png',
+  ),
+
+  '3' : const User(
+    id: '2',
+    name: 'Fernanda',
+    email: 'fernanda@aluno.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2020/11/22/22/17/male-5768177_960_720.png',
+  ),
+
+  '4' : const User(
+    id:'3',
+    name: 'Marcela',
+    email: 'marcela@aluno.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2021/03/02/08/52/woman-6061868_960_720.jpg',
+  ),
+};
+
